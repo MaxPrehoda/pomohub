8fef71811b83fb9474b1c60eabbe1017ef5998b4
MaxPrehoda <maxprehoda@gmail.com>
2022-10-07 10:07:36 -0700

    Added min max and close

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -21,7 +21,16 @@ function AppBar() {
           <img className="w-6 mr-1 ml-1" src={Icon} alt="Icon of pomo" />
           <p className="text-xs font-semibold pt-1">Pomo Hub</p>
         </div>
-        <div className="inline-flex -mt-1">MIN ICON | MAX ICON | CLOSE ICON</div>
+        <div className="inline-flex -mt-1">
+          <button onClick={window.Main.Minimize} className="undraggable md:px-4 lg:px-3 pt-1 hover:bg-gray-300">
+            &#8211;
+          </button>
+          <button onClick={handleToggle} className="undraggable px-6 lg:px-5 pt-1 hover:bg-gray-300">
+            {isMaximize ? '\u2752' : '⃞'}
+          </button>
+          <button onClick={window.Main.Close} className="undraggable px-4 pt-1 hover:bg-red-500 hover:text-white">
+            &#10005;
+          </button></div>
       </div>
     </div>
   );
