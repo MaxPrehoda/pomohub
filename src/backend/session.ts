4d37bfac2b92f098433bbcc8de43090ab689bdfe
lkwokcs <81600208+Lawrence-Kwok@users.noreply.github.com>
2022-10-07 09:02:55 -1000

    added cycle data property to sessions

--- src/backend/session.ts
+++ src/backend/session.ts
@@ -5,10 +5,13 @@ export default class PomoSession {
 
   isRunning: boolean;
 
+  cycleArray: Array<object>;
+
   constructor(startingDate: Date) {
     this.startingDate = startingDate;
     this.numberOfCyclesCompleted = 0;
     this.isRunning = false;
+    this.cycleArray = [];
   }
 
   start() {
