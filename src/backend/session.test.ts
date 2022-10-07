8cd94e6f330ad32645838b314a48606ab1bf3b03
asa-leholland <asaleholland@gmail.com>
2022-10-07 11:17:00 -0700

    #34 WIP: remove number of expected cycles

--- src/backend/session.test.ts
+++ src/backend/session.test.ts
@@ -2,17 +2,12 @@ import PomoSession from './session';
 
 describe('Session', () => {
   it('should be associated with the date when the Session was started', () => {
-    const session = new PomoSession(new Date(), 4);
+    const session = new PomoSession(new Date());
     expect(session.startingDate).toBeDefined();
   });
 
-  it('should be associated with the number of cycles that the Session will have', () => {
-    const session = new PomoSession(new Date(), 4);
-    expect(session.numberOfCycles).toBe(4);
-  });
-
   it('should be able to be started and stopped', () => {
-    const session = new PomoSession(new Date(), 4);
+    const session = new PomoSession(new Date());
     session.start();
     expect(session.isRunning).toBe(true);
     session.stop();
