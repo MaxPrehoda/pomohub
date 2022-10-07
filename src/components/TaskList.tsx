def46b06b486c7da2542d03e4fa74cc1bf214398
MaxPrehoda <maxprehoda@gmail.com>
2022-10-07 00:37:43 -0700

    Fixed wonky text on rows created by AddTask()

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -18,7 +18,7 @@ function TaskList() {
   const addTask = (): void => {
     const newTask = { taskName: task };
     setTodoList([...todoList, newTask]);
-    setTask(''); //clear inputs
+    setTask(''); // clear inputs
   };
 
   const completeTask = (taskNameToDelete: string): void => {
@@ -36,13 +36,13 @@ function TaskList() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="inline w-8 h-8 pb-1.5"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
           />
         </svg>
@@ -51,35 +51,23 @@ function TaskList() {
       {todoList.map((task: Tasks, key: number) => {
         return <Task key={key} task={task} completeTask={completeTask} />;
       })}
-      <div className="bg-zinc-700 p-2 rounded-md text-left hover:opacity-60">
-        <input className="mr-1" type="checkbox"></input> Take out trash
-      </div>
-      <div className="bg-zinc-700 p-2 rounded-md text-left hover:opacity-60">
-        <input className="mr-2" type="checkbox"></input>Mock up text
-      </div>
-      <div className="bg-zinc-700 p-2 rounded-md text-left hover:opacity-60">
-        <input className="mr-2" type="checkbox"></input>More of that
-      </div>
-      <div className="bg-zinc-700 p-2 rounded-md text-left hover:opacity-60">
-        <input className="mr-2" type="checkbox"></input>And even more
-      </div>
       <div className="bg-transparent p-2 rounded-md text-center hover:opacity-60 border-2 border-zinc-700">
         <input
-          className="bg-transparent"
+          className="bg-transparent outline-none"
           name="task"
           placeholder="Task Name"
           value={task}
           onChange={handleChange}
-        ></input>
+        />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="inline w-6 h-6 pr-1"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
         <button onClick={addTask}>Add Task</button>
       </div>
