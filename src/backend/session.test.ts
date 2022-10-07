814d57c3f35c75e2fbb9917b7e62f6a8bffa83aa
asa-leholland <asaleholland@gmail.com>
2022-10-07 09:50:45 -0700

    #34 FIX: fix broken test runner

--- src/backend/session.test.ts
+++ src/backend/session.test.ts
@@ -5,4 +5,9 @@ describe('Session', () => {
     const session = new PomoSession(new Date(), 4);
     expect(session.startingDate).toBeDefined();
   });
+
+  it('should be associated with the number of cycles that the Session will have', () => {
+    const session = new PomoSession(new Date(), 4);
+    expect(session.numberOfCycles).toBe(4);
+  });
 });
