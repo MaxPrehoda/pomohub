e4f2e3549b002d098173af550be95eddb9d3f57d
MaxPrehoda <maxprehoda@gmail.com>
2022-10-06 22:06:52 -0700

    Added ability to add and delete tasks in tasklist

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,28 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, ChangeEvent, useState } from 'react';
+import Task from './Task';
 
 function TaskList() {
-    interface ITask {
+    interface Tasks {
         taskName: string;
-        deadline: number;
     }
 
     const [task, setTask] = useState<string>("");
-    const [deadline, setDealine] = useState<number>(0);
-    const [todoList, setTodoList] = useState<ITask[]>([]);
+    const [todoList, setTodoList] = useState<Tasks[]>([]);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
         if (event.target.name === "task") {
             setTask(event.target.value);
-        } else {
-            setDealine(Number(event.target.value));
         }
     };
 
     const addTask = (): void => {
-        const newTask = { taskName: task, deadline: deadline };
+        const newTask = { taskName: task };
         setTodoList([...todoList, newTask]);
-        setTask("");
-        setDealine(0);
+        setTask(""); //clear inputs
     };
 
     const completeTask = (taskNameToDelete: string): void => {
@@ -40,12 +36,16 @@ function TaskList() {
                 <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
             </svg>
                 <h1 className="text-2xl inline text-white pl-1">Tasks</h1></span>
+            {todoList.map((task: Tasks, key: number) => {
+                return <Task key={key} task={task} completeTask={completeTask} />;
+            })}
             <div className='bg-zinc-700 p-2 rounded-md text-left hover:opacity-60'><input className='mr-1' type="checkbox"></input> Take out trash</div>
             <div className='bg-zinc-700 p-2 rounded-md text-left hover:opacity-60'><input className='mr-2' type="checkbox"></input>Mock up text</div>
             <div className='bg-zinc-700 p-2 rounded-md text-left hover:opacity-60'><input className='mr-2' type="checkbox"></input>More of that</div>
             <div className='bg-zinc-700 p-2 rounded-md text-left hover:opacity-60'><input className='mr-2' type="checkbox"></input>And even more</div>
             <div className='bg-transparent p-2 rounded-md text-center hover:opacity-60 border-2 border-zinc-700'>
-                <input className='bg-transparent' placeholder='Task Name'></input><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline w-6 h-6 pr-1">
+                <input className='bg-transparent' name='task' placeholder='Task Name' value={task}
+                    onChange={handleChange}></input><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="inline w-6 h-6 pr-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                 </svg><button onClick={addTask}>Add Task</button></div>
         </div>
