9a3b1b5c46fd23672d3d85a6ca6fed4f9e0cb76b
asa-leholland <asaleholland@gmail.com>
2022-10-08 14:11:41 -0700

    FEAT: add component to export current session data

---
+++ src/components/ExportButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import usePomoSession from '../backend/session';
+
+function ExportSessionButton() {
+  const { getSessionSummary } = usePomoSession();
+
+  const exportSessionSummary = () => {
+    const summary = getSessionSummary();
+    const summaryString = JSON.stringify(summary);
+    const blob = new Blob([summaryString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.download = 'pomo-session-summary.json';
+    link.href = url;
+    link.click();
+  };
+
+  return (
+    <button className="bg-green-400 rounded-md pl-2 mr-2 pr-2 pt-2 pb-2" onClick={exportSessionSummary}>
+      Export Session Summary
+    </button>
+  );
+}
+
+export default ExportSessionButton;
