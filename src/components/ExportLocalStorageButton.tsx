bc4594199da939ee03ef2e323435be4f0fecb18c
asa-leholland <asaleholland@gmail.com>
2022-10-08 14:32:56 -0700

    WIP: work with disabled

--- src/components/ExportLocalStorageButton.tsx
+++ src/components/ExportLocalStorageButton.tsx
@@ -15,8 +15,11 @@ function ExportLocalStorageButton() {
   };
 
   return (
-    <button className="bg-green-400 rounded-md pl-2 mr-2 pr-2 pt-2 pb-2" onClick={exportLocalStorage}>
-      Export Local Storage (This works, Max make this UI better)
+    <button
+      className="bg-purple-400 rounded-md pl-2 mr-2 pr-2 pt-2 pb-2 disabled:opacity-50"
+      onClick={exportLocalStorage}
+    >
+      Export Local Storage (This works, this button can be moved/re-UI'd)
     </button>
   );
 }
--- src/components/ExportSessionButton.tsx
+++ src/components/ExportSessionButton.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import usePomoSession from '../backend/session';
 
 function ExportSessionButton() {
-  const { getSessionSummary } = usePomoSession();
+  const { pomoSessionData, getSessionSummary } = usePomoSession();
+
+  const isButtonDisabled = Object.keys(pomoSessionData).length === 0;
 
   const exportSessionSummary = () => {
     const summary = getSessionSummary();
@@ -16,8 +18,12 @@ function ExportSessionButton() {
   };
 
   return (
-    <button className="bg-green-400 rounded-md pl-2 mr-2 pr-2 pt-2 pb-2" onClick={exportSessionSummary}>
-      Export Session Summary  (This does not work, Max make this UI better)
+    <button
+      className="bg-purple-400 rounded-md pl-2 mr-2 pr-2 pt-2 pb-2 disabled:opacity-50"
+      onClick={exportSessionSummary}
+      disabled={isButtonDisabled}
+    >
+      Export Session Summary (this does not work yet)
     </button>
   );
 }
