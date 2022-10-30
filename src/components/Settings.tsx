4055501e3220689a570747ca2fe0eade8bc09819
MaxPrehoda <maxprehoda@gmail.com>
2022-10-30 02:01:11 -0700

    fixed close settings modal button

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -246,24 +246,24 @@ function SettingsModal({ settingsHandler2 }: Props) {
               <div>
                 <RangeSlider {...breakSliderProps} classes="additional-css-classes" />
               </div>
+              <button
+                onClick={settingsHandler2}
+                className="bg-zinc-50 bg-opacity-20 hover:bg-zinc-50 hover:bg-opacity-30 text-white font-semibold py-2 px-4 rounded ml-[200px] mt-8 -ml-0 border-2 border-pink-400"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 inline"
+                >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                Close Settings
+              </button>
             </div>
           </div>
-          <button
-            onClick={settingsHandler2}
-            className="bg-pink-400 hover:bg-pink-300 text-white font-semibold py-2 px-4 rounded ml-[200px] mt-8"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6 inline"
-            >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-            Close Settings
-          </button>
         </div>
       </div>
     </div>
